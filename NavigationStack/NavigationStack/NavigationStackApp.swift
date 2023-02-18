e043a46e7b4d938b04e985e9898f897cd51472bd
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2023-02-18 19:23:35 +0300

    Add NavigationStack project

---
+++ NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by emre argana on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
