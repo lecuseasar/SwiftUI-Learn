eedf7d7246e8b4dad901885fdf7784783b92a73e
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2023-03-11 20:20:08 +0300

    Create Swift UI blank project

---
+++ SwiftUIForm/SwiftUIForm/SwiftUIFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormApp.swift
+//  SwiftUIForm
+//
+//  Created by emre argana on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
