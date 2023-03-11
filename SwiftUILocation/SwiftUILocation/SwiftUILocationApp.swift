da129435677d58474ad9826d91c4787d471e9f0f
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2023-03-11 21:58:48 +0300

    Create Swift UI user location project

---
+++ SwiftUILocation/SwiftUILocation/SwiftUILocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocationApp.swift
+//  SwiftUILocation
+//
+//  Created by emre argana on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
